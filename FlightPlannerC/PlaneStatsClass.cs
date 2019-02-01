8f37475f9b640778bdd9d42c6d1bbf831ca68aee
David Parsons <D_A_Parsons@outlook.com>
2019-02-01 22:03:17 +0000

    Split files into own classes

---
+++ FlightPlannerC/PlaneStatsClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPlannerC
+{
+
+    class PlaneStats
+    {
+        private Plane plane;
+
+        public Plane Specs
+        {
+            get { return plane; }
+            set { plane = value; }
+        }
+
+        public int CruiseSpeed { get { return plane.CruiseSpeed; } }
+        public int MaxRange { get { return plane.MaxRange; } }
+        public int MaxFuel { get { return plane.MaxFuel; } }
+        public int EmptyWeight { get { return plane.EmptyWeight; } }
+        public int MaxTakeOff { get { return plane.MaxTakeOff; } }
+        public int CruisingAlt { get { return plane.CruisingAlt; } }
+    }
+
+}
